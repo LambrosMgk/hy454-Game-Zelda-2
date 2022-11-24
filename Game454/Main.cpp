795be8ae353c795c186291b0c0682bd077fa13f9
LambrosMgk <116386147+LambrosMgk@users.noreply.github.com>
2022-11-24 15:24:47 +0200

    Phase 1.0.1

--- Game454/Main.cpp
+++ Game454/Main.cpp
@@ -8,7 +8,7 @@
 #include <sstream>
 
 using namespace std;
-
+/*returns a vector<vector<int>> of indeces that corresponde to a TileSet*/
 vector<vector<int>> ReadTextMap(string TileMapFileName)
 {
 	ifstream TileMap(TileMapFileName);
@@ -42,6 +42,8 @@ vector<vector<int>> ReadTextMap(string TileMapFileName)
 int main(int argc, char* argv[])
 {
 	ALLEGRO_DISPLAY* display = NULL;
+	ALLEGRO_BITMAP* bitmap = NULL;
+	vector<vector<int>> TileMap;
 
 	if (!al_init())
 	{
@@ -56,9 +58,18 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	ReadTextMap("UnitTests\\map1_Kachelebene 1.csv");
-	al_clear_to_color(al_map_rgb(61, 61, 61));
+	TileMap = ReadTextMap("UnitTests\\map1_Kachelebene 1.csv");
+	if (TileMap.size() != 0)
+	{
+		bitmap = al_create_bitmap(640, 480);
+		al_draw_bitmap(bitmap, 0, 0, 0);
+		al_set_target_bitmap(bitmap);
+	}
+	//al_clear_to_color(al_map_rgb(61, 61, 61));
 	al_flip_display();
+	/*Copies or updates the front and back buffers so that what has been drawn previously on the currently selected display becomes visible 
+	on screen. Pointers to the special back and front buffer bitmaps remain valid and retain their semantics as back and front buffers 
+	respectively, although their contents may have changed.*/
 	al_rest(10.0);
 	al_destroy_display(display);
 
